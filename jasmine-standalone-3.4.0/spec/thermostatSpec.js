8127832a4dd4d6da0a04b8d9fb28e836cba7996e
karlo <deguzmankarlo@gmail.com>
2019-09-18 14:25:46 +0100

    Add features: turn thermostat up and down, minimum temperature enforced, switch power saving mode on and off

--- jasmine-standalone-3.4.0/spec/thermostatSpec.js
+++ jasmine-standalone-3.4.0/spec/thermostatSpec.js
@@ -10,4 +10,37 @@ describe('Thermostat', function() {
   it('starts at 20 degrees', function() {
     expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
+
+  it('increases in temperature with up', function() {
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
+  it('decreases in temperature with down', function() {
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+
+  it('has a minimum temperature of 10 degrees', function() {
+    for (var i = 0; i < 11; i++) {
+      thermostat.down();
+    };
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
+
+  it('has power saving mode on by default', function() {
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
+
+  it('can switch power saving mode off', function() {
+    thermostat.switchPowerSavingModeOff();
+    expect(thermostat.isPowerSavingModeOn()).toBe(false);
+  });
+
+  it('can switch power saving mode on', function() {
+    thermostat.switchPowerSavingModeOff();
+    expect(thermostat.isPowerSavingModeOn()).toBe(false);
+    thermostat.switchPowerSavingModeOn();
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
 });
--- jasmine-standalone-3.4.0/src/thermostat.js
+++ jasmine-standalone-3.4.0/src/thermostat.js
@@ -1,9 +1,38 @@
 'use strict';
 
 function Thermostat() {
+  this.powerSavingMode = true;
+  this.MINIMUM_TEMPERATURE = 10;
   this.temperature = 20;
 };
 
 Thermostat.prototype.getCurrentTemperature = function() {
   return this.temperature;
+};
+
+Thermostat.prototype.up = function() {
+  this.temperature++;
+};
+
+Thermostat.prototype.down = function() {
+  if (this.isMinimumTemperature()) {
+    return;
+  }
+  this.temperature--;
+};
+
+Thermostat.prototype.isMinimumTemperature = function() {
+  return this.temperature === this.MINIMUM_TEMPERATURE;
+};
+
+Thermostat.prototype.isPowerSavingModeOn = function() {
+  return this.powerSavingMode === true;
+};
+
+Thermostat.prototype.switchPowerSavingModeOff = function() {
+  this.powerSavingMode = false;
+};
+
+Thermostat.prototype.switchPowerSavingModeOn = function() {
+  this.powerSavingMode = true;
 };
