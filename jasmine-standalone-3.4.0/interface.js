2542d691329c3fd8ffac89af87afba1054cea919
karlo <deguzmankarlo@gmail.com>
2019-09-20 11:14:36 +0100

    Add current temperature information from weather API and fix thermostat temperature to change colour depending on energy usage

--- jasmine-standalone-3.4.0/interface.js
+++ jasmine-standalone-3.4.0/interface.js
@@ -29,8 +29,22 @@ $(document).ready(function() {
     updateTemperature();
   })
 
+  $('#current-city').change(function() {
+    displayWeather();
+  })
+
+  function displayWeather() {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=';
+    var city = $('#current-city').val();
+    var token = '&appid=3658356c0ec42de64979f3b8318daf45';
+    var units = '&units=metric';
+    $.get(url + city + token + units, function(data) {
+      $('#current-temperature').text(Math.round(data.main.temp))
+    })
+  }
+
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
-    $('#temperature').attr('style', thermostat.energyUsage());
+    $('#temperature').attr('class', thermostat.energyUsage());
   }
 })
